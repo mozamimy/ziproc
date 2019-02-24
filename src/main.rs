d97b7a5ddc23e7890d6822c1771e277e30b1e72e
mozamimy <alice@mozami.me>
2019-02-24 10:40:30 +0900

    Just fork a process with fork(2), execv(3) and waitpid(2)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use libc;
+use std;
+use std::ffi::CString;
+
 fn main() {
-    println!("hello")
+    let pid;
+    unsafe {
+        pid = libc::fork();
+    }
+
+    if pid == 0 {
+        let command = CString::new("/usr/bin/echo").unwrap();
+        let arg = CString::new("Hello world.").unwrap();
+        let status;
+        unsafe {
+            status = libc::execv(command.as_ptr(), vec![command.as_ptr(), arg.as_ptr(), std::ptr::null()].as_ptr())
+        }
+        println!("s: {:?}", status)
+    } else {
+        let status = 0 as *mut i32;
+        unsafe {
+            libc::waitpid(pid, status, 0);
+        }
+        println!("status: {:?}", status);
+    }
 }
